08230cfddf44ace56ddb335ce6f5e7e469c6f23e
Alexander Pronin <pronin.alx@gmail.com>
2016-04-10 15:16:23 +0300

    Final commit

--- Challenge Starter/AppPreferenceChallengeStarter/ViewController.swift
+++ Challenge Starter/AppPreferenceChallengeStarter/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let backgroundColorType = userDefaults.integerForKey("BackgroundColor")
+        switch backgroundColorType {
+        case 0:
+            view.backgroundColor = UIColor.redColor()
+        case 1:
+            view.backgroundColor = UIColor.whiteColor()
+        case 2:
+            view.backgroundColor = UIColor.blueColor()
+        default:
+            view.backgroundColor = UIColor.yellowColor()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
